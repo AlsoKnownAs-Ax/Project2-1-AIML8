e4c683c81701be7535fb4124a00e2234956c5495
Tristan <62807768+Tristan-Dormans@users.noreply.github.com>
2024-11-29 11:27:39 +0100

    Uncommented VisionConeComponent

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/VisionConeComponent.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/VisionConeComponent.cs
@@ -1,18 +1,18 @@
-// using UnityEngine;
-// using Unity.MLAgents.Sensors;
+using UnityEngine;
+using Unity.MLAgents.Sensors;
 
-// public class VisionConeComponent : SensorComponent
-//     {
-//         public VisionCone visionCone;
+public class VisionConeComponent : SensorComponent
+    {
+        public VisionCone visionCone;
         
-//         /// <summary>
-//         /// Creates a BasicSensor.
-//         /// </summary>
-//         /// <returns></returns>
+        /// <summary>
+        /// Creates a BasicSensor.
+        /// </summary>
+        /// <returns></returns>
     
-//         public override ISensor[] CreateSensors()
-//         {
-//             visionCone =  new VisionCone();
-//             return new ISensor[]{visionCone};
-//         }
-//     }
+        public override ISensor[] CreateSensors()
+        {
+            visionCone =  new VisionCone();
+            return new ISensor[]{visionCone};
+        }
+    }
