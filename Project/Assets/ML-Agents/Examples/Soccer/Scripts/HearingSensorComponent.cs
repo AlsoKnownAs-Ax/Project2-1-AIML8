83844585e20fceedda28caaee0402475183abec2
--Ax- <andreiamzu@yahoo.com>
2025-01-09 18:15:08 +0100

    🐛 [Fix] Fix Hearing Sensor attachment

--- Project/Assets/ML-Agents/Examples/Soccer/Scripts/HearingSensorComponent.cs
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/HearingSensorComponent.cs
@@ -4,10 +4,24 @@ using Unity.MLAgents.Sensors;
 public class HearingSensorComponent : SensorComponent
 {
     public HearingSensor hearingSensor;
+
+    private void Awake()
+    {
+        InitializeHearingSensor();
+    }
+
+    private void InitializeHearingSensor()
+    {
+        hearingSensor = gameObject.GetComponent<HearingSensor>() ?? gameObject.AddComponent<HearingSensor>();
+    }
     
     public override ISensor[] CreateSensors()
     {
-        hearingSensor = gameObject.GetComponent<HearingSensor>() ?? gameObject.AddComponent<HearingSensor>();
-        return new ISensor[]{hearingSensor};
+        if (hearingSensor == null)
+        {
+            InitializeHearingSensor();
+        }
+
+        return new ISensor[] { hearingSensor };
     }
 }
