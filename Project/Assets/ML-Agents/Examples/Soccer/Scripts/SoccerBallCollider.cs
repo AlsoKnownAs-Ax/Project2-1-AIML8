ef2d5eb6859c167979fd2bba5914d0fffba676f8
Tristan <62807768+Tristan-Dormans@users.noreply.github.com>
2025-01-22 21:51:20 +0100

    Added missing SoccerBallCollider.cs, tested hearingSensor

---
+++ Project/Assets/ML-Agents/Examples/Soccer/Scripts/SoccerBallCollider.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+public class SoccerBallCollider : MonoBehaviour
+{
+    public SoccerEnvController area;
+    [HideInInspector]
+    public HearingSensor hearingSensor;
+    private float _inRange = 40f;
+    void Start()
+    {
+    }
+    void Update() 
+    {
+        foreach (var item in area.AgentsList) 
+        {
+            if(Vector3.Distance(gameObject.transform.position, item.Agent.transform.position) < _inRange)
+            {
+                var hearingSensorComponent = item.Agent.GetComponent<HearingSensorComponent>();
+                if (hearingSensorComponent?.hearingSensor != null) 
+                {
+                    // Get the ball's collider component
+                    var ballCollider = GetComponent<Collider>();
+                    if (ballCollider != null)
+                    {
+                        hearingSensorComponent.hearingSensor.OnTriggerEnter(ballCollider);
+                    }
+                }
+            }
+        }
+    }
+}
